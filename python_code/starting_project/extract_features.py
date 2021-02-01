faf233d185e59313729805cb4793365cb161db4e
Orestis Apostolou <orestisapostolou@yahoo.gr>
2021-02-01 18:33:25 +0200

    Finished features extraction. Added a features list description

--- python_code/starting_project/extract_features.py
+++ python_code/starting_project/extract_features.py
@@ -1,11 +1,14 @@
 import pandas as pd
 import math
+import numpy as np
 # import matplotlib.pyplot as plt
 from separate_measurements import Separation
 from r_peaks_detection import PeaksDetection
 import statistics
 import fft_transform
 import pyhrv.nonlinear
+import pyhrv.time_domain
+import time
 
 
 ULTRA_SHORT_TIME_WINDOW = 30  # half minute
@@ -24,31 +27,89 @@ class Extraction:
     def extract_short_features(self):
         """The method for extracting ECG features in an ultra short window."""
         current_data = self.data[10]
-        r_peaks = PeaksDetection(fs=self.fs, data_series=self.data[8]).detect_peaks()
+        print(len(self.data))
+        r_peaks = PeaksDetection(fs=self.fs, data_series=self.data[10]).detect_peaks()
         differences = []
         for i in range(1, len(r_peaks)):
             differences.append(r_peaks[i] - r_peaks[i-1])
         normalized_differences = self.__normalized_differences(differences=differences)
 
+        features_list_dir = []
+        names = ["SDRR", "Average Heart Rate", "SDNN", "SDSD", "SDANN", "SDNNI", "pNN50", "RMSSD", "HTI",
+                 "HR max - HR min", "VLF Energy", "VLF Energy Percentage", "LF Energy", "LF Energy Percentage",
+                 "HF Energy", "HF Energy Percentage", "Poincare sd1", "Poincare sd2", "Poincare ratio",
+                 "Poincare Ellipsis Area", "Mean Approximate Entropy", "Approximate Entropy std",
+                 "Mean Sample Entropy", "Sample Entropy std", "Short-term Fluctuation", "Long-term Fluctuation"]
+        units = ["msec", "bpm", "msec", "msec", "msec", "msec", "NaN", "msec", "NaN", "bpm", "NaN", "NaN", "NaN",
+                 "NaN", "NaN", "NaN", "msec", "msec", "NaN", "msec^2", "NaN", "NaN", "NaN", "NaN", "NaN", "NaN"]
+        calculation_times_app = [0.00069, 0.00032, 0.00062, 0.00128, 0.13853, 0.13853, 0.00052, 0.00073,
+                                 0.00016, 0.00004, 0.00559, 0.00559, 0.00559, 0.00559, 0.00559, 0.00559,
+                                 0.04680, 0.04680, 0.04680, 0.04680, 0.74070, 0.74070, 1.31020, 1.31020,
+                                 0.07168, 0.07168]
+        dict = {"Names": names, "Units": units, "Approximate calculation times": calculation_times_app}
+        df = pd.DataFrame(data=dict)
+        # print(df)
+        # df.to_csv(path_or_buf="./features_list.csv")
+
+        time_durations = []
+
+        tic = time.perf_counter()
         res = self.__sdrr(differences=differences)
+        toc = time.perf_counter()
         print(f"SDRR is {round(res, 2)} msec.")
+        time_durations.append(toc - tic)
+
+        tic = time.perf_counter()
         res2 = self.__average_heart_rate(differences=differences)
         print(f"Average heart rate is {round(res2, 1)} bpm.")
+        toc = time.perf_counter()
+        time_durations.append(toc - tic)
+
+        tic = time.perf_counter()
         res3 = self.__sdnn(normalized_differences=normalized_differences)
         print(f"SDNN is {round(res3, 2)} msec.")
+        toc = time.perf_counter()
+        time_durations.append(toc - tic)
+
+        tic = time.perf_counter()
         res3b = self.__sdsd(normalized_differences=normalized_differences)
         print(f"SDSD is {round(res3b, 2)} msec.")
+        toc = time.perf_counter()
+        time_durations.append(toc - tic)
+
+        tic = time.perf_counter()
         res4, res5 = self.__sdann__and_sdnni(data_series=current_data)
         print(f"SDANN is {round(res4, 2)} msec.")
         print(f"SDNNI is {round(res5, 2)} msec.")
+        toc = time.perf_counter()
+        time_durations.append((toc - tic) / 2)
+        time_durations.append((toc - tic) / 2)
+
+        tic = time.perf_counter()
         res6 = self.__pnn50(normalized_differences=normalized_differences)
         print(f"pNN50 percentage is {round(res6*100,1)}%")
+        toc = time.perf_counter()
+        time_durations.append(toc - tic)
+
+        tic = time.perf_counter()
         res7 = self.__rmssd(normalized_differences=normalized_differences)
         print(f"RMSSD is {round(res7, 2)} msec.")
+        toc = time.perf_counter()
+        time_durations.append(toc - tic)
+
+        tic = time.perf_counter()
         res7b = self.__hti(normalized_differences=normalized_differences)
         print(f"HTI is {round(res7b, 3)}")
+        toc = time.perf_counter()
+        time_durations.append(toc - tic)
+
+        tic = time.perf_counter()
         res8 = self.__hrmaxmin(differences=differences)
         print(f"HR max - HR min is {round(res8, 1)} bpm.")
+        toc = time.perf_counter()
+        time_durations.append(toc - tic)
+
+        tic = time.perf_counter()
         fourier_transform = fft_transform.FFTTransform(fs=self.fs, data_series=current_data)
         res9a, res9b = fourier_transform.vlf_band()
         print(f"VLF band energy is {res9a}.")
@@ -59,11 +120,44 @@ class Extraction:
         res11a, res11b = fourier_transform.hf_band()
         print(f"HF band energy is {res11a}.")
         print(f"HF band energy is {round(res11b * 100, 2)}% of the full energy.")
+        toc = time.perf_counter()
+        for _ in range(0, 6):
+            time_durations.append((toc - tic) / 6)
+
+        tic = time.perf_counter()
         res12a, res12b, res12c, res12d = self.__poincare(normalized_differences=normalized_differences)
         print(f"Poincare values - sd1: {round(res12a, 2)} ms, sd2: {round(res12b, 2)} ms, sd ratio: {round(res12c, 2)}"
               f", ellipse area {round(res12d, 2)}, ms^2.")
+        toc = time.perf_counter()
+        for _ in range(0, 4):
+            time_durations.append((toc - tic) / 4)
 
+        tic = time.perf_counter()
+        res13a, res13b = self.__approximate_entropy_mean_and_std(data_array=current_data.array, m=2, r=0.04, window=5)
+        print(f"Mean approximate entropy is {round(res13a, 4)}.")
+        print(f"Standard deviation of approximate entropy is {round(res13b, 4)}.")
+        toc = time.perf_counter()
+        time_durations.append((toc - tic) / 2)
+        time_durations.append((toc - tic) / 2)
 
+        tic = time.perf_counter()
+        res14a, res14b = self.__sample_entropy_mean_and_std(data_array=current_data.array, r=0.04, window=5)
+        toc = time.perf_counter()
+        print(f"Mean sample entropy is {round(res14a, 4)}.")
+        print(f"Standard deviation of sample entropy is {round(res14b, 4)}.")
+        toc = time.perf_counter()
+        time_durations.append((toc - tic) / 2)
+        time_durations.append((toc - tic) / 2)
+
+        tic = time.perf_counter()
+        res15 = pyhrv.nonlinear.dfa(normalized_differences, show=False)
+        res15a = res15["dfa_alpha1"]
+        res15b = res15["dfa_alpha2"]
+        print(f"Alpha value of the short-term fluctuations is {res15a}.")
+        print(f"Alpha value of the long-term fluctuations is {res15b}.")
+        toc = time.perf_counter()
+        time_durations.append((toc - tic) / 2)
+        time_durations.append((toc - tic) / 2)
 
     def extract_ultra_short_features(self):
         """The method for extracting ECG features in an ultra short window."""
@@ -171,7 +265,8 @@ class Extraction:
         diff = []
         for i in range(0, len(normalized_differences) - 1):
             diff.append(abs(normalized_differences[i] - normalized_differences[i + 1]))
-        return statistics.stdev(diff) / self.fs * 1000
+        diff = diff / self.fs * 1000
+        return statistics.stdev(diff)
 
 
     def __rmssd(self, normalized_differences):
@@ -240,3 +335,68 @@ class Extraction:
         difs = normalized_differences / self.fs
         res = pyhrv.nonlinear.poincare(nni=difs, show=False)
         return res["sd1"], res["sd2"], res["sd_ratio"], res["ellipse_area"]
+        # sd1 is equal to (0.5 * rmssd) ^ 0.5
+
+    def __approximate_entropy_mean_and_std(self, data_array, m, r, window: int) -> tuple:
+        """Returns the mean and standard deviation of approximate energy, in a tuple."""
+
+        def approximate_entropy(data_arr, m2, r1):
+            data_arr = np.array(data_arr)
+            n = data_arr.shape[0]
+
+            def _phi(m1):
+                z = n - m1 + 1.0
+                x = np.array([data_arr[ii:ii + m1] for ii in range(int(z))])
+                x2 = np.repeat(x[:, np.newaxis], 1, axis=2)
+                c = np.sum(np.absolute(x - x2).max(axis=2) <= r1, axis=0) / z
+                return np.log(c).sum() / z
+
+            return abs(_phi(m2 + 1) - _phi(m2))
+
+        window_samples = int(window * self.fs)
+        approximate_energy = []
+        for i in range(0, len(data_array), window_samples):
+            if i+window_samples < len(data_array):
+                approximate_energy.append(approximate_entropy(data_arr=data_array[i:i+window_samples], m2=m, r1=r))
+            else:
+                approximate_energy.append(approximate_entropy(data_arr=data_array[i:], m2=m, r1=r))
+        mn = statistics.mean(approximate_energy)
+        sd = statistics.stdev(approximate_energy)
+        return mn, sd
+
+    def __sample_entropy_mean_and_std(self, data_array, r, window: int):
+        """Returns the mean and standard deviation of sample energy, in a tuple."""
+
+        def sampen(data, m, r1):
+            n = len(data)
+            b = 0.0
+            a = 0.0
+
+            # Split time series and save all templates of length m
+            xmi = np.array([data[i: i + m] for i in range(n - m)])
+            xmj = np.array([data[i: i + m] for i in range(n - m + 1)])
+
+            # Save all matches minus the self-match, compute B
+            b = np.sum([np.sum(np.abs(xmii - xmj).max(axis=1) <= r1) - 1 for xmii in xmi])
+
+            # Similar for computing A
+            m += 1
+            xm = np.array([data[i: i + m] for i in range(n - m + 1)])
+
+            a = np.sum([np.sum(np.abs(xmi - xm).max(axis=1) <= r1) - 1 for xmi in xm])
+
+            # Return SampEn
+            return -np.log(a / b)
+
+        window_samples = int(window * self.fs)
+        sample_entropy = []
+        for i in range(0, len(data_array), window_samples):
+            if i + window_samples < len(data_array):
+                sample_entropy.append(sampen(data=data_array[i:i + window_samples], r1=r, m=2))
+            else:
+                sample_entropy.append(sampen(data=data_array[i:], r1=r, m=2))
+        mn = statistics.mean(sample_entropy)
+        sd = statistics.stdev(sample_entropy)
+        return mn, sd
+
+
