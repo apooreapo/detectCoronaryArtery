85be553215a76111eac25a213b88a9562b4574c0
Orestis Apostolou <orestisapostolou@yahoo.gr>
2021-02-02 02:29:45 +0200

    Added some more functionality in features extraction.

--- python_code/starting_project/extract_features.py
+++ python_code/starting_project/extract_features.py
@@ -24,15 +24,11 @@ class Extraction:
         self.data = self.short_measurements["data"]
         self.fs = self.short_measurements["fs"]
 
-    def extract_short_features(self):
-        """The method for extracting ECG features in an ultra short window."""
-        current_data = self.data[10]
-        print(len(self.data))
-        r_peaks = PeaksDetection(fs=self.fs, data_series=self.data[10]).detect_peaks()
-        differences = []
-        for i in range(1, len(r_peaks)):
-            differences.append(r_peaks[i] - r_peaks[i-1])
-        normalized_differences = self.__normalized_differences(differences=differences)
+    def extract_short_features(self, print_message=False) -> dict:
+        """The method for extracting ECG features in an ultra short window.
+        Return a dictionary with the extracted features of the file"""
+
+        # Construct the features_list.csv with the extracted features characteristics.
 
         features_list_dir = []
         names = ["SDRR", "Average Heart Rate", "SDNN", "SDSD", "SDANN", "SDNNI", "pNN50", "RMSSD", "HTI",
@@ -46,118 +42,186 @@ class Extraction:
                                  0.00016, 0.00004, 0.00559, 0.00559, 0.00559, 0.00559, 0.00559, 0.00559,
                                  0.04680, 0.04680, 0.04680, 0.04680, 0.74070, 0.74070, 1.31020, 1.31020,
                                  0.07168, 0.07168]
-        dict = {"Names": names, "Units": units, "Approximate calculation times": calculation_times_app}
-        df = pd.DataFrame(data=dict)
+        feature_dict = {"Names": names, "Units": units, "Approximate calculation times": calculation_times_app}
+        df = pd.DataFrame(data=feature_dict)
         # print(df)
         # df.to_csv(path_or_buf="./features_list.csv")
 
-        time_durations = []
-
         tic = time.perf_counter()
-        res = self.__sdrr(differences=differences)
-        toc = time.perf_counter()
-        print(f"SDRR is {round(res, 2)} msec.")
-        time_durations.append(toc - tic)
 
-        tic = time.perf_counter()
-        res2 = self.__average_heart_rate(differences=differences)
-        print(f"Average heart rate is {round(res2, 1)} bpm.")
-        toc = time.perf_counter()
-        time_durations.append(toc - tic)
+        column_titles = names
+        column_titles.append("File Title")
+        column_titles.append("Has CAD")
 
-        tic = time.perf_counter()
-        res3 = self.__sdnn(normalized_differences=normalized_differences)
-        print(f"SDNN is {round(res3, 2)} msec.")
-        toc = time.perf_counter()
-        time_durations.append(toc - tic)
+        res_dict = {}
+        for name in column_titles:
+            res_dict[name] = []
 
-        tic = time.perf_counter()
-        res3b = self.__sdsd(normalized_differences=normalized_differences)
-        print(f"SDSD is {round(res3b, 2)} msec.")
-        toc = time.perf_counter()
-        time_durations.append(toc - tic)
+        for index in range(0, 3):
+            current_data = self.data[index]
+            r_peaks = PeaksDetection(fs=self.fs, data_series=current_data).detect_peaks()
+            differences = []
+            for i in range(1, len(r_peaks)):
+                differences.append(r_peaks[i] - r_peaks[i-1])
+            normalized_differences = self.__normalized_differences(differences=differences)
 
-        tic = time.perf_counter()
-        res4, res5 = self.__sdann__and_sdnni(data_series=current_data)
-        print(f"SDANN is {round(res4, 2)} msec.")
-        print(f"SDNNI is {round(res5, 2)} msec.")
-        toc = time.perf_counter()
-        time_durations.append((toc - tic) / 2)
-        time_durations.append((toc - tic) / 2)
+            # time_durations = []
 
-        tic = time.perf_counter()
-        res6 = self.__pnn50(normalized_differences=normalized_differences)
-        print(f"pNN50 percentage is {round(res6*100,1)}%")
-        toc = time.perf_counter()
-        time_durations.append(toc - tic)
+            # tic = time.perf_counter()
+            res = self.__sdrr(differences=differences)
+            res_dict[column_titles[0]].append(res)
 
-        tic = time.perf_counter()
-        res7 = self.__rmssd(normalized_differences=normalized_differences)
-        print(f"RMSSD is {round(res7, 2)} msec.")
-        toc = time.perf_counter()
-        time_durations.append(toc - tic)
+            # toc = time.perf_counter()
+            if print_message:
+                print(f"SDRR is {round(res, 2)} msec.")
+            # time_durations.append(toc - tic)
 
-        tic = time.perf_counter()
-        res7b = self.__hti(normalized_differences=normalized_differences)
-        print(f"HTI is {round(res7b, 3)}")
-        toc = time.perf_counter()
-        time_durations.append(toc - tic)
+            # tic = time.perf_counter()
+            res2 = self.__average_heart_rate(differences=differences)
+            res_dict[column_titles[1]].append(res2)
+            if print_message:
+                print(f"Average heart rate is {round(res2, 1)} bpm.")
+            # toc = time.perf_counter()
+            # time_durations.append(toc - tic)
 
-        tic = time.perf_counter()
-        res8 = self.__hrmaxmin(differences=differences)
-        print(f"HR max - HR min is {round(res8, 1)} bpm.")
-        toc = time.perf_counter()
-        time_durations.append(toc - tic)
+            # tic = time.perf_counter()
+            res3 = self.__sdnn(normalized_differences=normalized_differences)
+            res_dict[column_titles[2]].append(res3)
+            if print_message:
+                print(f"SDNN is {round(res3, 2)} msec.")
+            # toc = time.perf_counter()
+            # time_durations.append(toc - tic)
 
-        tic = time.perf_counter()
-        fourier_transform = fft_transform.FFTTransform(fs=self.fs, data_series=current_data)
-        res9a, res9b = fourier_transform.vlf_band()
-        print(f"VLF band energy is {res9a}.")
-        print(f"VLF band energy is {round(res9b * 100, 2)}% of the full energy.")
-        res10a, res10b = fourier_transform.lf_band()
-        print(f"LF band energy is {res10a}.")
-        print(f"LF band energy is {round(res10b * 100, 2)}% of the full energy.")
-        res11a, res11b = fourier_transform.hf_band()
-        print(f"HF band energy is {res11a}.")
-        print(f"HF band energy is {round(res11b * 100, 2)}% of the full energy.")
-        toc = time.perf_counter()
-        for _ in range(0, 6):
-            time_durations.append((toc - tic) / 6)
+            # tic = time.perf_counter()
+            res3b = self.__sdsd(normalized_differences=normalized_differences)
+            res_dict[column_titles[3]].append(res3b)
+            if print_message:
+                print(f"SDSD is {round(res3b, 2)} msec.")
+            # toc = time.perf_counter()
+            # time_durations.append(toc - tic)
 
-        tic = time.perf_counter()
-        res12a, res12b, res12c, res12d = self.__poincare(normalized_differences=normalized_differences)
-        print(f"Poincare values - sd1: {round(res12a, 2)} ms, sd2: {round(res12b, 2)} ms, sd ratio: {round(res12c, 2)}"
-              f", ellipse area {round(res12d, 2)}, ms^2.")
-        toc = time.perf_counter()
-        for _ in range(0, 4):
-            time_durations.append((toc - tic) / 4)
+            # tic = time.perf_counter()
+            res4, res5 = self.__sdann__and_sdnni(data_series=current_data)
+            res_dict[column_titles[4]].append(res4)
+            res_dict[column_titles[5]].append(res5)
+            if print_message:
+                print(f"SDANN is {round(res4, 2)} msec.")
+                print(f"SDNNI is {round(res5, 2)} msec.")
+            # toc = time.perf_counter()
+            # time_durations.append((toc - tic) / 2)
+            # time_durations.append((toc - tic) / 2)
 
-        tic = time.perf_counter()
-        res13a, res13b = self.__approximate_entropy_mean_and_std(data_array=current_data.array, m=2, r=0.04, window=5)
-        print(f"Mean approximate entropy is {round(res13a, 4)}.")
-        print(f"Standard deviation of approximate entropy is {round(res13b, 4)}.")
-        toc = time.perf_counter()
-        time_durations.append((toc - tic) / 2)
-        time_durations.append((toc - tic) / 2)
+            # tic = time.perf_counter()
+            res6 = self.__pnn50(normalized_differences=normalized_differences)
+            res_dict[column_titles[6]].append(res6)
+            if print_message:
+                print(f"pNN50 percentage is {round(res6*100,1)}%")
+            # toc = time.perf_counter()
+            # time_durations.append(toc - tic)
 
-        tic = time.perf_counter()
-        res14a, res14b = self.__sample_entropy_mean_and_std(data_array=current_data.array, r=0.04, window=5)
-        toc = time.perf_counter()
-        print(f"Mean sample entropy is {round(res14a, 4)}.")
-        print(f"Standard deviation of sample entropy is {round(res14b, 4)}.")
-        toc = time.perf_counter()
-        time_durations.append((toc - tic) / 2)
-        time_durations.append((toc - tic) / 2)
+            # tic = time.perf_counter()
+            res7 = self.__rmssd(normalized_differences=normalized_differences)
+            res_dict[column_titles[7]].append(res7)
+            if print_message:
+                print(f"RMSSD is {round(res7, 2)} msec.")
+            # toc = time.perf_counter()
+            # time_durations.append(toc - tic)
 
-        tic = time.perf_counter()
-        res15 = pyhrv.nonlinear.dfa(normalized_differences, show=False)
-        res15a = res15["dfa_alpha1"]
-        res15b = res15["dfa_alpha2"]
-        print(f"Alpha value of the short-term fluctuations is {res15a}.")
-        print(f"Alpha value of the long-term fluctuations is {res15b}.")
+            # tic = time.perf_counter()
+            res7b = self.__hti(normalized_differences=normalized_differences)
+            res_dict[column_titles[8]].append(res7b)
+            if print_message:
+                print(f"HTI is {round(res7b, 3)}")
+            # toc = time.perf_counter()
+            # time_durations.append(toc - tic)
+
+            # tic = time.perf_counter()
+            res8 = self.__hrmaxmin(differences=differences)
+            res_dict[column_titles[9]].append(res8)
+            if print_message:
+                print(f"HR max - HR min is {round(res8, 1)} bpm.")
+            # toc = time.perf_counter()
+            # time_durations.append(toc - tic)
+
+            # tic = time.perf_counter()
+            fourier_transform = fft_transform.FFTTransform(fs=self.fs, data_series=current_data)
+            res9a, res9b = fourier_transform.vlf_band()
+            res_dict[column_titles[10]].append(res9a)
+            res_dict[column_titles[11]].append(res9b)
+            if print_message:
+                print(f"VLF band energy is {res9a}.")
+                print(f"VLF band energy is {round(res9b * 100, 2)}% of the full energy.")
+            res10a, res10b = fourier_transform.lf_band()
+            res_dict[column_titles[12]].append(res10a)
+            res_dict[column_titles[13]].append(res10b)
+            if print_message:
+                print(f"LF band energy is {res10a}.")
+                print(f"LF band energy is {round(res10b * 100, 2)}% of the full energy.")
+            res11a, res11b = fourier_transform.hf_band()
+            res_dict[column_titles[14]].append(res11a)
+            res_dict[column_titles[15]].append(res11b)
+            if print_message:
+                print(f"HF band energy is {res11a}.")
+                print(f"HF band energy is {round(res11b * 100, 2)}% of the full energy.")
+            # toc = time.perf_counter()
+            # for _ in range(0, 6):
+            #     time_durations.append((toc - tic) / 6)
+
+            # tic = time.perf_counter()
+            res12a, res12b, res12c, res12d = self.__poincare(normalized_differences=normalized_differences)
+            res_dict[column_titles[16]].append(res12a)
+            res_dict[column_titles[17]].append(res12b)
+            res_dict[column_titles[18]].append(res12c)
+            res_dict[column_titles[19]].append(res12d)
+            if print_message:
+                print(f"Poincare values - sd1: {round(res12a, 2)} ms, sd2: {round(res12b, 2)} ms, sd ratio:"
+                      f" {round(res12c, 2)}"
+                      f", ellipse area {round(res12d, 2)}, ms^2.")
+            # toc = time.perf_counter()
+            # for _ in range(0, 4):
+            #     time_durations.append((toc - tic) / 4)
+
+            # tic = time.perf_counter()
+            res13a, res13b = self.__approximate_entropy_mean_and_std(data_array=current_data.array, m=2,
+                                                                     r=0.04, window=5)
+            res_dict[column_titles[20]].append(res13a)
+            res_dict[column_titles[21]].append(res13b)
+            if print_message:
+                print(f"Mean approximate entropy is {round(res13a, 4)}.")
+                print(f"Standard deviation of approximate entropy is {round(res13b, 4)}.")
+            # toc = time.perf_counter()
+            # time_durations.append((toc - tic) / 2)
+            # time_durations.append((toc - tic) / 2)
+
+            # tic = time.perf_counter()
+            res14a, res14b = self.__sample_entropy_mean_and_std(data_array=current_data.array, r=0.04, window=5)
+            res_dict[column_titles[22]].append(res14a)
+            res_dict[column_titles[23]].append(res14b)
+            # toc = time.perf_counter()
+            if print_message:
+                print(f"Mean sample entropy is {round(res14a, 4)}.")
+                print(f"Standard deviation of sample entropy is {round(res14b, 4)}.")
+            # toc = time.perf_counter()
+            # time_durations.append((toc - tic) / 2)
+            # time_durations.append((toc - tic) / 2)
+
+            # tic = time.perf_counter()
+            res15 = pyhrv.nonlinear.dfa(normalized_differences, show=False)
+            res15a = res15["dfa_alpha1"]
+            res15b = res15["dfa_alpha2"]
+            res_dict[column_titles[24]].append(res15a)
+            res_dict[column_titles[25]].append(res15b)
+            if print_message:
+                print(f"Alpha value of the short-term fluctuations is {res15a}.")
+                print(f"Alpha value of the long-term fluctuations is {res15b}.")
+            # toc = time.perf_counter()
+            # time_durations.append((toc - tic) / 2)
+            # time_durations.append((toc - tic) / 2)
+            res_dict[column_titles[26]].append(self.file)
+            res_dict[column_titles[27]].append(0)
         toc = time.perf_counter()
-        time_durations.append((toc - tic) / 2)
-        time_durations.append((toc - tic) / 2)
+        print(f"Total extraction time: {round(toc - tic, 3)} sec.")
+        return res_dict
 
     def extract_ultra_short_features(self):
         """The method for extracting ECG features in an ultra short window."""
