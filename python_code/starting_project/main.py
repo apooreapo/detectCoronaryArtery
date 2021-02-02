eabf93b50c58b9f652a7090ed9fbeab236cc603f
Orestis Apostolou <orestisapostolou@yahoo.gr>
2021-02-02 02:31:57 +0200

    Changed main.py

--- python_code/starting_project/main.py
+++ python_code/starting_project/main.py
@@ -1,4 +1,13 @@
 from extract_features import Extraction
+import glob
+import pandas as pd
 
+files = glob.glob("./samples/*.csv")
+dataframes = []
+for file in files:
+    dataframes.append(pd.DataFrame(Extraction(file).extract_short_features()))
+res = pd.concat(dataframes, ignore_index=True)
 
-Extraction("./16265.csv").extract_short_features()
+res.to_csv(path_or_buf="./example_features_output.csv")
+# print(pd.DataFrame(dicts[0]))
+# print(pd.DataFrame(dicts[1]))
