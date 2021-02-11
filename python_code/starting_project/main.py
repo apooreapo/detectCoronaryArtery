20916a694bc886bfb07272bcaf4ed7338a7c4ee4
Orestis Apostolou <orestisapostolou@yahoo.gr>
2021-02-11 15:27:02 +0200

    Added a starting dataSet in .csv and some python files for extraction

--- python_code/starting_project/main.py
+++ python_code/starting_project/main.py
@@ -1,11 +1,28 @@
+import sys
 from extract_features import Extraction
 import glob
 import pandas as pd
+import gc
 
-files = glob.glob("./samples/*.csv")
-dataframes = []
-for file in files:
-    dataframes.append(pd.DataFrame(Extraction(file).extract_short_features()))
-res = pd.concat(dataframes, ignore_index=True)
+# file_titles = "./samples/*.csv"
+# files = glob.glob(file_titles)
+# print(f"Total number of files: {len(files)}")
+# count = 1
+args = sys.argv
+if len(args) > 1:
+    file = args[1]
+    txt = file.split(sep='/')[2]
+    # print(f"File number {count} out of {len(files)}: {txt}")
+    myExtraction = Extraction(file)
+    # print(file)
+    # print(txt)
+    res = pd.DataFrame(myExtraction.extract_short_features())
+    # count += 1
+    output = "./extracted_features/final_extracted_"+txt
+    res.to_csv(path_or_buf=output)
+    del res
+    del myExtraction
+    gc.collect()
+# res = pd.concat(dataframes, ignore_index=True)
 
-res.to_csv(path_or_buf="./example_features_output2.csv")
+
